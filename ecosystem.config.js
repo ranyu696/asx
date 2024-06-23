ee1a3b38214fce25be4c7db00070cb7baf6a63c2
ranyu696 <ranyu696@gmail.com>
2024-06-23 16:29:24 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+    apps: [
+      {
+        name: 'asx',
+        script: 'node_modules/next/dist/bin/next',
+        args: 'start',
+        env: {
+          NODE_ENV: 'production',
+          PORT: 3003
+        }
+      }
+    ]
+  };
